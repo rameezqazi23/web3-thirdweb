90f0e6b840b735d224181fbd45bec1073b74f264
rameezqazi23 <rameez.qazi23@gmail.com>
2023-09-04 17:02:58 +0500

    creates folder structure and setting up tailwindcss, and now writing components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home, Profile } from "./pages";
+import { Navbar, Sidebar } from "./components";
+
 
 const App = () => {
 
   console.log("Hello from App.js")
   return (
-    <div>
-      <h1>ThirdWeb</h1>
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      <div className='sm:flex hidden mr-10'>
+        <Sidebar />
+      </div>
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
 
     </div>
   )
---
+++ client/src/pages/CampaignDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CampaignDetails = () => {
+  return (
+    <div>
+      Campaign Datails
+    </div>
+  )
+}
+
+export default CampaignDetails
